a129c7addf32a24489fb713a30a1be8d03cbc7e8
Radu Tarean <radutarean@gmail.com>
2024-11-06 14:17:09 +0200

    feat: add desktop bubble

--- main.js
+++ main.js
@@ -1,18 +1,25 @@
 function toggleShare() {
-  const footer = document.getElementById("footer");
-  footer.classList.toggle("share-active");
+  const displayWidth = window.innerWidth;
 
-  const author = document.getElementById("author");
-  author.classList.toggle("display-none");
+  if (displayWidth < 1024) {
+    const footer = document.getElementById("footer");
+    footer.classList.toggle("share-active");
 
-  const links = document.getElementById("links");
-  links.classList.toggle("display-none");
+    const author = document.getElementById("author");
+    author.classList.toggle("display-none");
 
-  const share = document.getElementById("share");
-  share.src = footer.classList.contains("share-active")
-    ? "./images/icon-share-white.svg"
-    : "./images/icon-share.svg";
+    const links = document.getElementById("links");
+    links.classList.toggle("display-none");
 
-  const shareButton = document.getElementById("share-button");
-  shareButton.classList.toggle("share-button-inactive");
+    const share = document.getElementById("share");
+    share.src = !footer.classList.contains("share-active")
+      ? "./images/icon-share-white.svg"
+      : "./images/icon-share.svg";
+
+    const shareButton = document.getElementById("share-button");
+    shareButton.classList.toggle("share-button-inactive");
+  } else {
+    const bubble = document.getElementById("bubble");
+    bubble.classList.toggle("display-none");
+  }
 }
